c4a4c2dc11a9db97b3ddb7742b3b9712bb3655d2
madlenkosii <jjuniorsikhosana@gmail.com>
2026-09-15 02:15:27 +0200

    added a function to generate OTP and send it to the user via email. .

--- backend/otpService.js
+++ backend/otpService.js
@@ -7,3 +7,18 @@ const users = new Map();
 function generateOTP() {
     return cryto.randomInt(0, 999999).toString().padStart(6, '0');
 }
+
+// Function to send a new OTP to a user
+function sendOTP(email) {
+    let user = users.get(email);
+    if (!user) {
+        user = { 
+            requests: [], 
+            oldOTPs: [] 
+        };
+        
+        users.set(email, user);
+    }
+}
+
+
